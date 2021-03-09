dfb4d3e847753b8449c6653668e14d93365ae8c0
Dylan Smith <dylan-smith@github.com>
2021-03-09 17:35:25 -0600

    removed unnecessary dist files

--- build.ps1
+++ build.ps1
@@ -1,3 +1,2 @@
 dotnet restore src/dotnet-xplat-action.sln
 dotnet build src/dotnet-xplat-action.sln -c Release --no-restore --output dist/
-dotnet test src/dotnet-xplat-action.sln -c Release --no-build --verbosity normal
